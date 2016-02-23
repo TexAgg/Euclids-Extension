ade1176ed34ad845ea73c71e0f04bf4acb7c68c4
Matt G <matt.gaikema@tamu.edu>
2016-02-23 15:22:20 -0600

    Bug fixes

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,44 +5,44 @@
  */
 function gcd_E(u,v){
     
-    var m = math.matrix([[1,0],[0,1]]);
-    var n = 0;
-    
-    while (v != 0){
-        var q = Math.floor(u/v);
-        m = math.multiply(m,math.matrix([[q,1],[1,0]]));
-        var temp = v;
-        v = u - q*v;
-        u = temp;
-        n++;
-    }
-    
-    var d = u;
-    var a = Math.pow(-1,n)*m.get([1,1]);
-    var b = Math.pow(-1,n+1)*m.get([0,1]);
- 
-    return {d,a,b};   
+	var m = math.matrix([[1,0],[0,1]]);
+	var n = 0;
+	
+	while (v != 0){
+		var q = Math.floor(u/v);
+		m = math.multiply(m,math.matrix([[q,1],[1,0]]));
+		var temp = v;
+		v = u - q*v;
+		u = temp;
+		n++;
+	}
+	
+	var d = u;
+	var a = Math.pow(-1,n)*m.get([1,1]);
+	var b = Math.pow(-1,n+1)*m.get([0,1]);
+	
+	return {d,a,b};   
 }
 
 /**
  * Displays the GCD and the relevant data
  */
 function on_submit(){
-    var u = parseInt(document.getElementById('u').value);
-    var v = parseInt(document.getElementById('v').value);
+    	var u = parseInt(document.getElementById('u').value);
+    	var v = parseInt(document.getElementById('v').value);
     
-    var result = gcd_E(u,v);
+   	var result = gcd_E(u,v);
     
-    var d = result.d;
-    var a = result.a;
-    var b = result.b;
+   	var d = result.d;
+    	var a = result.a;
+    	var b = result.b;
     
-    var output = document.getElementById('output');
+   	var output = document.getElementById('output');
     
-    var test = "a: " + a + "b: " + b + "d: " + d;
-    //console.log(test);
+    	var test = "a: " + a + "b: " + b + "d: " + d;
+    	//console.log(test);
     
-    output.innerHTML = test;
+    	output.innerHTML = test;
             
 }
 

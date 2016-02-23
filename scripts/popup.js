dea8ea0cbaa2e557578064ef9bb64ff25004d21d
Matt G <matt.gaikema@tamu.edu>
2016-02-23 14:08:03 -0600

    Documentation

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,3 @@
-//console.log(math.sqrt(-4));
-
 function gcd_E(u,v){
     
     var m = math.matrix([[1,0],[0,1]]);
@@ -31,49 +29,13 @@ function on_submit(){
     var a = result.a;
     var b = result.b;
     
-    //var output = document.createElement('div');
-    //output.setAttribute('class','output');
-    
     var output = document.getElementById('output');
     
     var test = "a: " + a + "b: " + b + "d: " + d;
     console.log(test);
     
     output.innerHTML = test;
-    
-    //output.innerHTML = '';
-    //output.innerHTML = test;
-    
-    //output.innerHTML = "<label for='a'>a:</label>" + "<input id='a' readonly></input>";
-    //var output1 = document.getElementById('a');
-    //output1.setAttribute('value',a); // = a;
-    
-    /*
-    var output1 = document.createElement("input");
-    output1.setAttribute('readonly','true');
-    
-    var output2 = document.createElement("input");
-    output2.setAttribute('readonly','true');
-    
-    var output3 = document.createElement("input");
-    output3.setAttribute('readonly','true');    
-    
-    output1.value = a;
-    output2.value = b;
-    output3.value = d;
-    
-    output1.innerHTML += '<label for=' + this + '>a:</label>';
-    
-    document.body.appendChild(output1);  
-    document.body.appendChild(output2);
-    document.body.appendChild(output3);
-    */
-    
-    //var len = document.body.childNodes.length;
-    //var bob = document.body.childNodes;
-    
-    //document.body.appendChild(output);
-    //document.body.replaceChild(output,output);                  
+            
 }
 
 var evel = document.getElementById('evel');
